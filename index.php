9ce6bd25db2bea0440852148d12b2c2de5936627
joshyywap <joshuaventura671@gmail.com>
2026-01-28 12:24:00 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Confession letter</title>
+   <button class="ph" onclick="window.open('https:/pornhub.com', '_blank')">CLICK FOR SURPRISE</button>
+
+</head>
+
+</body>
+</html>
